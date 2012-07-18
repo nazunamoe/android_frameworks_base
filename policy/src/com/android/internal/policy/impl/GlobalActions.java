8104f3e09340f40804ae9365f07319ac7b4a91ed
sonicxml <sonicxml@gmail.com>
2012-07-18 14:27:04 -0400

    Move SafeMode reboot option to the Reboot Menu
    Change-Id: I2e2ac87022b39d6190cb9b8445997ef1986c9c8b

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -281,8 +281,8 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 }
 
                 public boolean onLongPress() {
-                    mWindowManagerFuncs.rebootSafeMode(true);
-                    return true;
+//                    mWindowManagerFuncs.rebootSafeMode();
+                    return false;
                 }
 
                 public boolean showDuringKeyguard() {
@@ -499,9 +499,26 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 }
 
                 public boolean showBeforeProvisioning() {
-                    return true;
+                        return true;
                 }
             });
+        mItems.add(
+                new SinglePressAction(
+                        com.android.internal.R.drawable.ic_lock_reboot_safemode,
+                        R.string.eos_globalactions_reboot_safemode) {
+
+                    public void onPress() {
+                        mWindowManagerFuncs.rebootSafeMode();
+                    }
+
+                    public boolean showDuringKeyguard() {
+                        return true;
+                    }
+
+                    public boolean showBeforeProvisioning() {
+                        return true;
+                    }
+                });
     }
 
     private void prepareDialog() {
