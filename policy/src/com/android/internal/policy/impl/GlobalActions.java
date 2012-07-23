f1184c57f33e1cab083f74900d20dd67e50e012c
DvTonder <david.vantonder@gmail.com>
2012-07-23 17:16:04 -0400

    Framework: Add the ability to enable/disable use of Profiles
    This will remove the Profiles item from the Power menu when the user has
    selected to disable the use of System Profiles.
    
    TODO: Better handling of returning a default profile when requested
    
    Change-Id: I3c29b1bbd8f72c5432a1906a6b412d9412279c9f

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -146,6 +146,8 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
     private static int rebootIndex = 0;
     private Profile mChosenProfile;
 
+    private static final String SYSTEM_PROFILES_ENABLED = "system_profiles_enabled";
+
     /**
      * @param context everything needs a context :(
      */
@@ -389,25 +391,27 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
             mItems.add(mExpandDesktopModeOn);
         }
 
-        // next: profile
-        mItems.add(
-            new ProfileChooseAction() {
-                public void onPress() {
-                    createProfileDialog();
-                }
+        // next: profile - only shown if enabled, which is true by default
+        if (Settings.System.getInt(mContext.getContentResolver(), SYSTEM_PROFILES_ENABLED, 1) == 1) {
+            mItems.add(
+                new ProfileChooseAction() {
+                    public void onPress() {
+                        createProfileDialog();
+                    }
 
-                public boolean onLongPress() {
-                    return true;
-                }
+                    public boolean onLongPress() {
+                        return true;
+                    }
 
-                public boolean showDuringKeyguard() {
-                    return false;
-                }
+                    public boolean showDuringKeyguard() {
+                        return false;
+                    }
 
-                public boolean showBeforeProvisioning() {
-                    return false;
-                }
-            });
+                    public boolean showBeforeProvisioning() {
+                        return false;
+                    }
+                });
+        }
 
         // next: airplane mode
         if (mEnableAirplaneToggle) {
