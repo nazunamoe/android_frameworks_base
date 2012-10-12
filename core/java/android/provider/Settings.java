43dba7f14b71fa31dce7405e4bc0e6d88c220738
Sven Dawitz <sven@dawitz.de>
2012-10-12 17:42:37 +0200

    Add CmCircleBattery to StatusBar options (1/2)
    Cyanogenmod Circle Battery implementation
    - Done 100% in pure code, wont blow up the framework with countless png resources
    - View can be shown with or without percentage as text inside
    - Indicator turns holo-red below 15%.
    - All sizes are calculated on the fly and should work on all current and
      future screen resolutions. (needs testing)
    - All colors are read from the holo-theme, so changes by themers will be obeyed.
    - The lower part of the pictures tries to illustrate the charge animation.
    
    Screenshot: http://dl.dropbox.com/u/16186798/cm10/cm-circle-bat-mockup.png
    
    Patchset 2:
    - Level >= 95% is treated as 100% (see inline comment)
    - paint.setDither(true) should reduce artifacts on 16bit color depth devices
    - modified tablet-specific xml. alignment should fit now (not tested yet)
    - cleaned up some xml formating derps
    - fixed possible crash bug
    
    PS3: 97% seems better than 95% for full circle
    
    Patchset 4+5: lost track of patchsets.
    
    Patchset 6:
    The battery fill-up above 97% was a quick shot. In the end,
    we show the right percentage in text, but circle fills up, once 97% is
    reached. This also solves the issue, that circle with very tiny gap
    looks slightly unbalanced.
    
    Also gave in about code formating. Its for the greater good. (I still am
    convinced, the whole world is wrong about this :)
    
    Patchset 7:
    - Fix derp
    
    Change-Id: I3dfe0b4e69c545cb9bbad2d1461d154618c60e00
    
    Conflicts:
    
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/layout/status_bar.xml
    	packages/SystemUI/res/layout/system_bar_notification_area.xml
    	packages/SystemUI/res/layout/system_bar_notification_panel_title.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/DockBatteryController.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1818,25 +1818,37 @@ public final class Settings {
          */
         public static final String LIGHT_FILTER_INTERVAL = "light_filter_interval";
 
-       /**
-        * Whether to show the clock in status bar
-        * of the stock battery icon
-        * 0: don't show the clock
-        * 1: right side clock
-        * 2: center clock
-        * default: 1
-        * @hide
-        */
+        /**
+         * Display style of the status bar battery information
+         * 0: Display the stock battery information
+         * 1: Display cm battery percentage implementation / dont show stock icon
+         * 2: Display cm circle battery implementation without percentage
+         * 3: Display cm circle battery implementation with percentage
+         * 4: Hide the battery information
+         * default: 0
+         * @hide
+         */
+        public static final String STATUS_BAR_BATTERY = "status_bar_battery";
+
+        /**
+         * Whether to show the clock in status bar
+         * of the stock battery icon
+         * 0: don't show the clock
+         * 1: right side clock
+         * 2: center clock
+         * default: 1
+         * @hide
+         */
         public static final String STATUS_BAR_CLOCK = "status_bar_clock";
          
-       /**
-        * Display style of AM/PM next to clock in status bar
-        * 0: Normal display (Eclair stock)
-        * 1: Small display (Froyo stock)
-        * 2: No display (Gingerbread/ICS stock)
-        * default: 2
-        * @hide
-        */
+        /**
+         * Display style of AM/PM next to clock in status bar
+         * 0: Normal display (Eclair stock)
+         * 1: Small display (Froyo stock)
+         * 2: No display (Gingerbread/ICS stock)
+         * default: 2
+         * @hide
+         */
         public static final String STATUS_BAR_AM_PM = "status_bar_am_pm"; 
 
         /**
--- packages/SystemUI/src/com/android/systemui/statusbar/policy/DockBatteryController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/DockBatteryController.java
@@ -27,15 +27,14 @@ import android.database.ContentObserver;
 import android.os.BatteryManager;
 import android.os.Handler;
 import android.provider.Settings;
-import android.util.Slog;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.systemui.R;
 
-public class BatteryController extends BroadcastReceiver {
-    private static final String TAG = "StatusBar.BatteryController";
+public class DockBatteryController extends BroadcastReceiver {
+    private static final String TAG = "StatusBar.DockBatteryController";
 
     private Context mContext;
     private ArrayList<ImageView> mIconViews = new ArrayList<ImageView>();
@@ -47,26 +46,23 @@ public class BatteryController extends BroadcastReceiver {
      * BATTERY_STYLE_CIRCLE* cannot be handled in this controller, since we cannot get views from
      * statusbar here. Yet it is listed for completion and not to confuse at future updates
      * See CircleBattery.java for more info
-     *
-     * set to public to be reused by CircleBattery
      */
-    public  static final int BATTERY_STYLE_CIRCLE         = 2;
-    public  static final int BATTERY_STYLE_CIRCLE_PERCENT = 3;
+    private static final int BATTERY_STYLE_CIRCLE         = 2;
+    private static final int BATTERY_STYLE_CIRCLE_PERCENT = 3;
     private static final int BATTERY_STYLE_GONE           = 4;
 
-    private static final int BATTERY_ICON_STYLE_NORMAL      = R.drawable.stat_sys_battery;
-    private static final int BATTERY_ICON_STYLE_CHARGE      = R.drawable.stat_sys_battery_charge;
-    private static final int BATTERY_ICON_STYLE_NORMAL_MIN  = R.drawable.stat_sys_battery_min;
-    private static final int BATTERY_ICON_STYLE_CHARGE_MIN  = R.drawable.stat_sys_battery_charge_min;
+    private static final int BATTERY_ICON_STYLE_UNKNOWN = R.drawable.stat_sys_kb_battery_unknown;
+    private static final int BATTERY_ICON_STYLE_NORMAL = R.drawable.stat_sys_kb_battery;
+    private static final int BATTERY_ICON_STYLE_CHARGE = R.drawable.stat_sys_kb_battery_charge;
 
-    private static final int BATTERY_TEXT_STYLE_NORMAL  = R.string.status_bar_settings_battery_meter_format;
-    private static final int BATTERY_TEXT_STYLE_MIN     = R.string.status_bar_settings_battery_meter_min_format;
-
-    private boolean mBatteryPlugged = false;
+    private int mDockStatus = BatteryManager.DOCK_BATTERY_STATUS_UNKNOWN;
+    private boolean mDockPresent = false;
     private int mBatteryStyle;
-    private int mBatteryIcon = BATTERY_ICON_STYLE_NORMAL;
 
-    Handler mHandler;
+    private static final int BATTERY_TEXT_STYLE_MIN =
+            R.string.status_bar_settings_battery_meter_min_format;
+
+    private Handler mHandler;
 
     class SettingsObserver extends ContentObserver {
         SettingsObserver(Handler handler) {
@@ -79,14 +75,14 @@ public class BatteryController extends BroadcastReceiver {
                     Settings.System.STATUS_BAR_BATTERY), false, this);
         }
 
-        @Override public void onChange(boolean selfChange) {
+        @Override
+        public void onChange(boolean selfChange) {
             updateSettings();
         }
     }
 
-    public BatteryController(Context context) {
+    public DockBatteryController(Context context) {
         mContext = context;
-
         mHandler = new Handler();
 
         SettingsObserver settingsObserver = new SettingsObserver(mHandler);
@@ -109,59 +105,68 @@ public class BatteryController extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         final String action = intent.getAction();
         if (action.equals(Intent.ACTION_BATTERY_CHANGED)) {
-            final int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
-            mBatteryPlugged = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, 0) != 0;
+            final int level = intent.getIntExtra(BatteryManager.EXTRA_DOCK_LEVEL, 0);
+            mDockStatus = intent.getIntExtra(BatteryManager.EXTRA_DOCK_STATUS,
+                    BatteryManager.DOCK_BATTERY_STATUS_UNKNOWN);
+            mDockPresent = intent.getBooleanExtra(BatteryManager.EXTRA_DOCK_PRESENT,
+                    false);
+
             int N = mIconViews.size();
-            for (int i=0; i<N; i++) {
+            for (int i = 0; i < N; i++) {
                 ImageView v = mIconViews.get(i);
                 v.setImageLevel(level);
                 v.setContentDescription(mContext.getString(R.string.accessibility_battery_level,
                         level));
             }
+
             N = mLabelViews.size();
-            for (int i=0; i<N; i++) {
+            for (int i = 0; i < N; i++) {
                 TextView v = mLabelViews.get(i);
-                v.setText(mContext.getString(BATTERY_TEXT_STYLE_MIN,
-                        level));
+                v.setText(mContext.getString(BATTERY_TEXT_STYLE_MIN, level));
             }
+
             updateBattery();
         }
     }
 
     private void updateBattery() {
-        int mIcon = View.GONE;
-        int mText = View.GONE;
-        int mIconStyle = BATTERY_ICON_STYLE_NORMAL;
+        int icon = View.GONE;
+        int text = View.GONE;
+        int iconStyle = BATTERY_ICON_STYLE_UNKNOWN;
 
-        if (mBatteryStyle == BATTERY_STYLE_NORMAL) {
-            mIcon = (View.VISIBLE);
-            mIconStyle = mBatteryPlugged ? BATTERY_ICON_STYLE_CHARGE
-                    : BATTERY_ICON_STYLE_NORMAL;
-        } else if (mBatteryStyle == BATTERY_STYLE_PERCENT) {
-            mIcon = (View.VISIBLE);
-            mText = (View.VISIBLE);
-            mIconStyle = mBatteryPlugged ? BATTERY_ICON_STYLE_CHARGE_MIN
-                    : BATTERY_ICON_STYLE_NORMAL_MIN;
+        if (mDockPresent && (mBatteryStyle == BATTERY_STYLE_NORMAL || mBatteryStyle == BATTERY_STYLE_PERCENT)) {
+            icon = View.VISIBLE;
+
+            if (mBatteryStyle == BATTERY_STYLE_PERCENT) {
+                text = View.VISIBLE;
+            }
+
+            if (mDockStatus == BatteryManager.DOCK_BATTERY_STATUS_NOT_CHARGING) {
+                iconStyle = BATTERY_ICON_STYLE_NORMAL;
+            } else if (mDockStatus == BatteryManager.DOCK_BATTERY_STATUS_CHARGING) {
+                iconStyle = BATTERY_ICON_STYLE_CHARGE;
+            }
         }
 
         int N = mIconViews.size();
-        for (int i=0; i<N; i++) {
+        for (int i = 0; i < N; i++) {
             ImageView v = mIconViews.get(i);
-            v.setVisibility(mIcon);
-            v.setImageResource(mIconStyle);
+            v.setVisibility(icon);
+            v.setImageResource(iconStyle);
         }
+
         N = mLabelViews.size();
-        for (int i=0; i<N; i++) {
+        for (int i = 0; i < N; i++) {
             TextView v = mLabelViews.get(i);
-            v.setVisibility(mText);
+            v.setVisibility(text);
         }
     }
 
     private void updateSettings() {
         ContentResolver resolver = mContext.getContentResolver();
-
         mBatteryStyle = (Settings.System.getInt(resolver,
-                Settings.System.STATUS_BAR_BATTERY, 0));
+                Settings.System.STATUS_BAR_BATTERY, BATTERY_STYLE_NORMAL));
+
         updateBattery();
     }
 }
