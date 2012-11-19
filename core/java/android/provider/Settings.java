ada81483884c1b553b67230edb23943eaed55aec
Daniel <daxx4182@gmail.com>
2012-11-19 12:03:10 +0800

    Revert "this one is shit and needs remove"
    This reverts commit e5701fb7366eb29e224dc1d6aa5e2366526a61d5.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1883,16 +1883,6 @@ public final class Settings {
          */
         public static final String CUSTOM_CARRIER_LABEL = "custom_carrier_label";
 
-        /**
-         * Display style of the day of the week before clock in status bar
-         * 0: Normal display
-         * 1: Small display
-         * 2: No display
-         * default: 2
-         * @hide
-         */
-        public static final String STATUS_BAR_WEEKDAY = "status_bar_weekday";
-
        /**
         * Whether to show the clock in status bar
         * of the stock battery icon
