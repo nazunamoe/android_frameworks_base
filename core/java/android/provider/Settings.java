cf7ad9c037db63ab3205eb10a64ae11cd0ffa43f
Adam Fisch <adammfisch@gmail.com>
2012-11-20 13:53:19 -0600

    SystemUI: Battery Bar
    TODO: Still has same bug on sw600dp where centered-mirrored is off center
    
    Change-Id: I938ef890459c600406a989414381e42df6d89ae0
    
    Conflicts:
    
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2886,6 +2886,32 @@ public final class Settings {
         public static final String MODE_VOLUME_OVERLAY = "mode_volume_overlay";
 
         /**
+         * Whether to show the battery bar
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR = "statusbar_battery_bar";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_COLOR = "statusbar_battery_bar_color";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_THICKNESS = "statusbar_battery_bar_thickness";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_STYLE = "statusbar_battery_bar_style";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_ANIMATE = "statusbar_battery_bar_animate";
+
+        /**
          * Boolean value whether to link ringtone and notification volumes
          * 
          * @hide
