a32a0a1f0a82ecebd817537700bc337e7c2a1176
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2012-12-04 14:24:19 +0100

    Quicksettings: add favorite contact tile (2/2)
    PS2+3: fix NPE
    
    Change-Id: I08b8859fbef4f1ee71b3f6fb259afec3a3829199
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1953,6 +1953,13 @@ public final class Settings {
         public static final String QUICK_TOGGLES_PER_ROW = "quick_toggles_per_row";
 
         /**
+         * favorite contact for quick settings
+         *
+         * @hide
+         */
+        public static final String QUICK_TOGGLE_FAV_CONTACT = "quick_toggle_fav_contact";
+
+        /**
          * Ability to hide Screenshot away from Reboot Menu
          * @hide
          */
