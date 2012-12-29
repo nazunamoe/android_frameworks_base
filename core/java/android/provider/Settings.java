13af420006f36bbbe48503869a3fffa11dceca36
Daniel <daxx4182@gmail.com>
2012-12-29 12:57:24 +0800

    derp
    Change-Id: Idb1b18e7c0d1a28fdc14fad897949286efb8b88f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3305,6 +3305,13 @@ public final class Settings {
          * 0 = color fill, 1 = custom image, 2 = full transparent, 3 = default background
          * @hide
          */
+        public static final String LOCKSCREEN_BACKGROUND = "lockscreen_background";
+
+        /**
+         * Sets the lockscreen background style array helper
+         * 0 = color fill, 1 = custom image, 2 = full transparent, 3 = default background
+         * @hide
+         */
         public static final String LOCKSCREEN_BACKGROUND_VALUE = "lockscreen_background_VALUE";
 
         /**
