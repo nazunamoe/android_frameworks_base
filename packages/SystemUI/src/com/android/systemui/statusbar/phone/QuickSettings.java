122d361a9040b7028ad30e245fc55ecc5d378b05
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2013-01-08 21:35:46 +0100

    QuickSettings: fix vibrate and silence toggle not updating
    Change-Id: Ic8dd7b451fbe3552c9a9d33407849b60ddc9740a
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
@@ -302,6 +302,7 @@ public class QuickSettings {
                 null, null);
 
         new SettingsObserver(new Handler()).observe();
+        new SoundObserver(new Handler()).observe();
     }
 
     public void setBar(PanelBar bar) {
@@ -1875,6 +1876,27 @@ public class QuickSettings {
         @Override
         public void onChange(boolean selfChange) {
             updateSettings();
+        }
+    }
+
+    class SoundObserver extends ContentObserver {
+        SoundObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.Global
+                    .getUriFor(Settings.Global.MODE_RINGER),
+                    false, this);
+            mModel.refreshVibrateTile();
+            mModel.refreshSilentTile();
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            mModel.refreshVibrateTile();
+            mModel.refreshSilentTile();
         }
     }
 }
