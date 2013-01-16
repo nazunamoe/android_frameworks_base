d0531f8aa8077a8a04f8806fbdfdc38d47ecddf3
Jesús David <jedga_95@outlook.com>
2013-01-15 22:01:04 -0500

    Code stlye fixes

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -54,11 +54,6 @@ import com.android.internal.R;
  */
 public class Clock extends TextView {
 
-    public interface OnClockChangedListener
-    {
-        public abstract void onChange(CharSequence t);
-    }
-
     private boolean mAttached;
     private Calendar mCalendar;
     private String mClockFormatString;
@@ -84,10 +79,10 @@ public class Clock extends TextView {
 
     protected int mClockColor = com.android.internal.R.color.holo_blue_light;
 
-    private OnClockChangedListener clockChangeListener = null;
-    public void setOnClockChangedListener(OnClockChangedListener l)
-    {
-        clockChangeListener = l;
+    private OnClockChangedListener mClockChangedListener;
+
+    public interface OnClockChangedListener {
+        public abstract void onChange(CharSequence t);
     }
 
     class SettingsObserver extends ContentObserver {
@@ -131,6 +126,10 @@ public class Clock extends TextView {
         updateSettings();
     }
 
+    public void setOnClockChangedListener(OnClockChangedListener l){
+        mClockChangedListener = l;
+    }
+
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
@@ -165,8 +164,8 @@ public class Clock extends TextView {
         mCalendar.setTimeInMillis(System.currentTimeMillis());
         CharSequence seq = getSmallTime();
         setText(seq);
-        if (clockChangeListener != null) {
-            clockChangeListener.onChange(seq);
+        if (mClockChangedListener != null) {
+            mClockChangedListener.onChange(seq);
         }
     }
 
