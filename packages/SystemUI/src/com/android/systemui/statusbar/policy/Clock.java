ed4895e04687855837c0d6dece10b8f415119258
drcmda <drcmda@gmail.com>
2013-01-16 01:52:48 +0000

    status clock test
    Change-Id: I61127bff0fc6542ec6ecdc19079313982cb5a605

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -53,6 +53,12 @@ import com.android.internal.R;
  * minutes.
  */
 public class Clock extends TextView {
+
+    public interface OnClockChangedListener
+    {
+        public abstract void onChange(CharSequence t);
+    }
+
     private boolean mAttached;
     private Calendar mCalendar;
     private String mClockFormatString;
@@ -78,6 +84,17 @@ public class Clock extends TextView {
 
     protected int mClockColor = com.android.internal.R.color.holo_blue_light;
 
+    private OnClockChangedListener clockChangeListener = null;
+    public void setOnClockChangedListener(OnClockChangedListener l)
+    {
+        clockChangeListener = l;
+    }
+
+    class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
     public Clock(Context context) {
         this(context, null);
     }
@@ -90,10 +107,7 @@ public class Clock extends TextView {
         super(context, attrs, defStyle);
     }
 
-    @Override
-    protected void onAttachedToWindow() {
-        super.onAttachedToWindow();
-
+    public void startBroadcastReceiver() {
         if (!mAttached) {
             mAttached = true;
             IntentFilter filter = new IntentFilter();
@@ -115,6 +129,12 @@ public class Clock extends TextView {
         SettingsObserver settingsObserver = new SettingsObserver(new Handler());
         settingsObserver.observe();
         updateSettings();
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        startBroadcastReceiver();
     }
 
     @Override
@@ -143,10 +163,14 @@ public class Clock extends TextView {
 
     final void updateClock() {
         mCalendar.setTimeInMillis(System.currentTimeMillis());
-        setText(getSmallTime());
+        CharSequence seq = getSmallTime();
+        setText(seq);
+        if (clockChangeListener != null) {
+            clockChangeListener.onChange(seq);
+        }
     }
 
-    private final CharSequence getSmallTime() {
+    public final CharSequence getSmallTime() {
         Context context = getContext();
         boolean b24 = DateFormat.is24HourFormat(context);
         int res;
