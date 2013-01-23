e553c260c9e35e1affafcb6d5eae6991495eaceb
Lars Greiss <kufikugel@googlemail.com>
2013-01-23 02:36:28 +0100

    Framework: Recents RAM bar (1/2)
    due that the old rambar was resource consuming and as the settings rambar not really
    right in showing the truth of real RAM usage in android, I decided to go new ways.
    
    This commit changes our rambar (or better add a total new one) in following points:
    
    - we do not use any longer ther permanent observed RunningState machine
    - instead we read out /proc/meminfo (thanks steven for that idea)
    - rambar shows now the real memory usage
    - ppl have the possibility to show applications, cache and other ram usage as they like
    - deeper explanations I add how android mem managment works.
    - color picker for all rambar states
    - reset to default
    - updates only on actions on recents so that it is making sense
    - and a lot other
    
    cheers
    
    Change-Id: I2cb29655f92fcdacd8a71d6192224ec8964acc17

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3267,6 +3267,35 @@ public final class Settings {
         public static final String RECENTS_RAM_BAR = "recents_ram_bar";
 
         /**
+         * whether which Ram Usage Bar mode is used on recent switcher
+         * 0 = none, 1 = only app use, 2 = app and cache use, 3 = app, cache and system use
+         * @hide
+         */
+        public static final String RECENTS_RAM_BAR_MODE = "recents_ram_bar_mode";
+
+        /**
+         * Ram Usage Bar system mem color
+         *
+         * @hide
+         */
+        public static final String RECENTS_RAM_BAR_MEM_COLOR = "recents_ram_bar_mem_color";
+
+        /**
+         * Ram Usage Bar cached mem color
+         *
+         * @hide
+         */
+        public static final String RECENTS_RAM_BAR_CACHE_COLOR = "recents_ram_bar_cache_color";
+
+        /**
+         * Ram Usage Bar app mem color
+         *
+         * @hide
+         */
+        public static final String RECENTS_RAM_BAR_ACTIVE_APPS_COLOR = "recents_ram_bar_active_apps_color";
+
+
+        /**
          * @hide
          */
         public static final String NAV_HIDE_TIMEOUT = "nav_hide_timeout";
