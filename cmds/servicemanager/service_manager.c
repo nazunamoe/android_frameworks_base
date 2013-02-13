9b0a994021e72418908e052ff4570d15406f098f
Daniel <daxx4182@gmail.com>
2013-02-14 01:50:56 +0800

    Revert "Switch keystore to binder"
    This reverts commit 6b77645aa9ac51ce33ea67adba226aaf1a6e8846.

--- cmds/servicemanager/service_manager.c
+++ cmds/servicemanager/service_manager.c
@@ -48,7 +48,6 @@ static struct {
     { AID_RADIO, "simphonebook" },
     { AID_MEDIA, "common_time.clock" },
     { AID_MEDIA, "common_time.config" },
-    { AID_KEYSTORE, "android.security.keystore" },
 };
 
 void *svcmgr_handle;
