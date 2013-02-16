015e926b7cefac8ceeca644355f6fe1f152eefa4
Daniel <daxx4182@gmail.com>
2013-02-16 13:47:22 +0800

    Revert "remove leak WaitResult List in startActivityMayWait"
    This reverts commit b2d68005d328024005d586874220987024d48a5d.

--- services/java/com/android/server/am/ActivityStack.java
+++ services/java/com/android/server/am/ActivityStack.java
@@ -3230,7 +3230,6 @@ final class ActivityStack {
                         } catch (InterruptedException e) {
                         }
                     } while (!outResult.timeout && outResult.who == null);
-                    mWaitingActivityLaunched.remove(outResult);
                 } else if (res == ActivityManager.START_TASK_TO_FRONT) {
                     ActivityRecord r = this.topRunningActivityLocked(null);
                     if (r.nowVisible) {
@@ -3247,7 +3246,6 @@ final class ActivityStack {
                             } catch (InterruptedException e) {
                             }
                         } while (!outResult.timeout && outResult.who == null);
-                        mWaitingActivityVisible.remove(outResult);
                     }
                 }
             }
