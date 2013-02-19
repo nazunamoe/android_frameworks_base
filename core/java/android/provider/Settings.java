76436ae800a93805a33751cdaf5a61a4001c5529
Stevespear426 <stephen.k.spear@gmail.com>
2013-02-20 00:41:16 +0100

    FW: Navring Rework.
    PS2: Moves drawables and static strings to Frameworks, Removes
         some redundant code from RC.
    
    PS3: Needs awesome action update for now if we dont add it ill
         rebase.
    
    PS4: Rebase and clean up, plus I added the option to turn "off" the
          navring by setting all targets to null or none.
    
    PS5: added logic for custom icons
    
    PS6: forgot to add Icons to the settings observer
         adjust some long press logic
    
    PS7: Comment message clean up
    
    Change-Id: Iff157e4e49407f46284f52697befe6cc3d5a42d7
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/java/com/android/internal/util/aokp/AwesomeConstants.java
    	packages/SystemUI/src/com/android/systemui/SearchPanelView.java
    	packages/SystemUI/src/com/android/systemui/navbar/SysAction.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/key/ExtensibleKeyButtonView.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3160,6 +3160,24 @@ public final class Settings {
         public static final String STATUSBAR_BATTERY_BAR_THICKNESS = "statusbar_battery_bar_thickness";
 
         /**
+<<<<<<< HEAD
+=======
+         * Custom navring icons
+         *
+         * @hide
+         */
+        public static final String[] SYSTEMUI_NAVRING_ICON = new String[] {
+                "navring_icon_0",
+                "navring_icon_1",
+                "navring_icon_2",
+                "navring_icon_3",
+                "navring_icon_4",
+        };
+
+        /**
+         * Setting to enable volume options.
+         *
+>>>>>>> 7616d8b... FW: Navring Rework.
          * @hide
          */
         public static final String STATUSBAR_BATTERY_BAR_STYLE = "statusbar_battery_bar_style";
--- packages/SystemUI/src/com/android/systemui/SearchPanelView.java
+++ packages/SystemUI/src/com/android/systemui/SearchPanelView.java
@@ -84,7 +84,11 @@ import com.android.systemui.statusbar.CommandQueue;
 import com.android.systemui.statusbar.phone.PhoneStatusBar;
 import com.android.systemui.statusbar.tablet.StatusBarPanel;
 import com.android.systemui.statusbar.tablet.TabletStatusBar;
+<<<<<<< HEAD
 import com.android.systemui.navbar.SysAction;
+=======
+import com.android.systemui.aokp.AwesomeAction;
+>>>>>>> 7616d8b... FW: Navring Rework.
 import static com.android.internal.util.aokp.AwesomeConstants.*;
 import com.android.internal.util.aokp.NavRingHelpers;
 
