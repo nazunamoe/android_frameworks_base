ab80c52b2cabb15232a577591d16ff42656bd0a9
YamilGhazi <yghazi@gmail.com>
2013-02-20 06:57:41 +0100

    UI change w/o reboot and navbar height
    Change-Id: I017c1ccb1d4c87fb166f1e744a36690a2150468f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3525,6 +3525,13 @@ public final class Settings {
         */
         public static final String TORCH_STATE = "torch_state";
 
+        /**
+         * User Interface State
+         * 1 = Rebuild UI, resets to 0 automatically
+         * @hide
+         */
+        public static final String USER_INTERFACE_STATE = "user_interface_state";
+
        /**
         * Lefty mode
         * @hide
