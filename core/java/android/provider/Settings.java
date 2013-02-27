59d244c90278afe358a35153ad1f38c4fc364728
Muhammed Nazim <mnm9994u@gmail.com>
2013-02-27 12:03:51 -0500

    Configurable Notification Behavior [2/2]
    PS 2 : Cleanup
    
    Credit: Michele Primavera
    
    Change-Id: I7693b05c6938e979e7d7735c0eff3a5018b81967

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3762,6 +3762,12 @@ public final class Settings {
         public static final String HEADSET_CONNECT_PLAYER = "headset_connect_player";
 
         /**
+         * Notifications Expand Behavior
+         * @hide
+         */
+        public static final String NOTIFICATIONS_BEHAVIOUR = "notifications_behaviour";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
