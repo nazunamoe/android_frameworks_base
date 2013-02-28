a967c126b0da8a1e5473c9231229d404fa8874cc
Jubakuba <Jubakuba@gmail.com>
2013-02-28 07:17:17 -0700

    Framework: CustomToggle [WIP]
    Early Early Early stages.
    Probably going to need A LOT of help here.
    
    Also, Yes.  You're ALL being kanged <3
    
    So far...SteveSpear426.
    More WILL Follow.
    
    PS4: Thanks to SteveSpear for Finding My Derp.
    PS6: Fix States && LongClick Logic
    PS7: Whoops.  Let's pretend PS6 never happened.
    PS8: Added Some Options / Fixed State at Boot (maybe)
    PS9: Actually set at boot (per option)
    PS10: Option to Change Action Logic
    PS11: More Options
    PS12: Set On Boot Fix For "Basic" Toggle
    PS13: Bothering me.  I'll get digging this weekend...
            Just a quick avoidance of runnable if not
            needed.
    PS14: Refactored to follow the new NavBar UI in ROMControl.
    PS15: commit message derp.
    PS16: Rebase against dependcies
    PS17: CustomState presists across death
    PS18: Derp
    PS19: Spacing errors
    PS20: Rebased
    PS21: Revise Option Default Per RC Update
    
    Change-Id: I80f157d1a8d68ad0b21028aa7d730025e2ceea04

--- packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
@@ -99,6 +99,7 @@ public class ToggleManager {
     public static final String STATUSBAR_TOGGLE = "STATUSBAR";
     public static final String SCREENSHOT_TOGGLE = "SCREENSHOT";
     public static final String REBOOT_TOGGLE = "REBOOT";
+    public static final String CUSTOM_TOGGLE = "CUSTOM";
 
     private int mStyle;
     private boolean mShowRebootOnLock = true;
@@ -159,6 +160,7 @@ public class ToggleManager {
             toggleMap.put(STATUSBAR_TOGGLE, StatusbarToggle.class);
             toggleMap.put(SCREENSHOT_TOGGLE, ScreenshotToggle.class);
             toggleMap.put(REBOOT_TOGGLE, RebootToggle.class);
+            toggleMap.put(CUSTOM_TOGGLE, CustomToggle.class);
             // toggleMap.put(BT_TETHER_TOGGLE, null);
         }
         return toggleMap;
