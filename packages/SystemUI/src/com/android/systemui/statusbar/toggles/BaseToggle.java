AOKP/frameworks_base

Toggle rewrite (1/2)
TODO:

    * cleanup code formatting
    * add hooks to tablet classes
    * fix clock spacing on tile

Change-Id: I80ef0c3f1cbe8e0d35c7a4e4111ad22b96196963

packages/SystemUI/src/com/android/systemui/statusbar/toggles/BaseToggle.java
@@ -1,0 +1,221 @@
+
+package com.android.systemui.statusbar.toggles;
+
+import android.app.ActivityManagerNative;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.internal.statusbar.IStatusBarService;
+import com.android.systemui.R;
+import com.android.systemui.statusbar.phone.QuickSettingsTileView;
+
+import java.util.ArrayList;
+
+public abstract class BaseToggle
+        implements OnClickListener, OnLongClickListener {
+
+    public static final String TAG = "Toggle";
+
+    protected Context mContext;
+
+    protected int mStyle;
+
+    private Drawable mIconDrawable = null;
+    private CharSequence mLabelText = null;
+    private int mTextSize = 12;
+
+    protected CompoundButton mToggleButton = null;
+    protected TextView mLabel = null;
+    protected ImageView mIcon = null;
+    private int mIconId = -1;
+
+    protected ArrayList<BroadcastReceiver> mRegisteredReceivers = new ArrayList<BroadcastReceiver>();
+
+    protected Handler mHandler;
+    private Runnable mUpdateViewRunnable = new Runnable() {
+        @Override
+        public void run() {
+            updateView();
+        }
+    };
+
+    public BaseToggle() {
+    }
+
+    protected void init(Context c, int style) {
+        mContext = c;
+        mStyle = style;
+        mHandler = new Handler();
+        setTextSize(XzibitToggler.getTextSize(mContext));
+        scheduleViewUpdate();
+    }
+
+    protected final void setTextSize(int s) {
+        mTextSize = s;
+    }
+
+    protected final void setInfo(final String label, final int resId) {
+        setLabel(label);
+        setIcon(resId);
+    }
+
+    protected final void setLabel(final String label) {
+        mLabelText = label;
+    }
+
+    protected final void setLabel(final int labelRes) {
+        mLabelText = mContext.getText(labelRes);
+    }
+
+    protected final void setIcon(int resId) {
+        if (resId == mIconId) {
+            // a little cache action
+            return;
+        }
+        mIconDrawable = null;
+        mIconDrawable = mContext.getResources().getDrawable(resId);
+        mIconId = resId;
+    }
+
+    protected final void setIcon(Drawable d) {
+        mIconDrawable = d;
+        mIconId = -1;
+    }
+
+    protected void cleanup() {
+        mHandler.removeCallbacks(mUpdateViewRunnable);
+        for (BroadcastReceiver br : mRegisteredReceivers) {
+            mContext.unregisterReceiver(br);
+        }
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        return true;
+    }
+
+    protected final void collapseStatusBar() {
+        try {
+            IStatusBarService sb = IStatusBarService.Stub.asInterface(ServiceManager
+                    .getService(Context.STATUS_BAR_SERVICE));
+            sb.collapsePanels();
+        } catch (RemoteException e) {
+        }
+    }
+
+    protected final void dismissKeyguard() {
+        try {
+            ActivityManagerNative.getDefault().dismissKeyguardOnNextActivity();
+        } catch (RemoteException e) {
+        }
+    }
+
+    public QuickSettingsTileView createTileView() {
+        QuickSettingsTileView quick = (QuickSettingsTileView)
+                View.inflate(mContext, R.layout.toggle_tile, null);
+        quick.setVisibility(View.VISIBLE);
+        quick.setOnClickListener(this);
+        quick.setOnLongClickListener(this);
+        mLabel = (TextView) quick.findViewById(R.id.label);
+        mIcon = (ImageView) quick.findViewById(R.id.icon);
+        return quick;
+    }
+
+    public View createTraditionalView() {
+        View view = View.inflate(mContext, R.layout.toggle_traditional, null);
+        mLabel = (TextView) view.findViewById(R.id.label);
+        mIcon = (ImageView) view.findViewById(R.id.icon);
+        view.setOnClickListener(this);
+        view.setOnLongClickListener(this);
+        return view;
+
+    }
+
+    protected final void scheduleViewUpdate() {
+        mHandler.removeCallbacks(mUpdateViewRunnable);
+        mHandler.postDelayed(mUpdateViewRunnable, 100);
+    }
+
+    protected final void startActivity(String a) {
+        startActivity(new Intent(a));
+    }
+
+    protected final void startActivity(Intent i) {
+        collapseStatusBar();
+        dismissKeyguard();
+        mContext.startActivityAsUser(i, new UserHandle(UserHandle.USER_CURRENT));
+    }
+
+    protected final void registerBroadcastReceiver(BroadcastReceiver r, IntentFilter f) {
+        if (r == null) {
+            return;
+        }
+        mRegisteredReceivers.add(r);
+        mContext.registerReceiver(r, f);
+    }
+
+    protected void updateView() {
+        if (mStyle == XzibitToggler.STYLE_SWITCH) {
+
+        } else if (mStyle == XzibitToggler.STYLE_TILE) {
+
+            if (mLabel != null) {
+                mLabel.setText(mLabelText);
+                mLabel.setVisibility(View.VISIBLE);
+                // if (mIconDrawable != null) {
+                // mLabel.setCompoundDrawablesWithIntrinsicBounds(null,
+                // mIconDrawable, null, null);
+                // }
+                mLabel.setTextSize(1, mTextSize);
+            }
+            if (mIcon != null) {
+                if (mIconDrawable != null) {
+                    mIcon.setImageDrawable(mIconDrawable);
+                }
+            }
+
+        } else if (mStyle == XzibitToggler.STYLE_TRADITIONAL) {
+            if (mIcon != null) {
+                if (mIconDrawable != null) {
+                    mIcon.setImageDrawable(mIconDrawable);
+                }
+            }
+        }
+    }
+
+    // Remove the double quotes that the SSID may contain
+    public static String removeDoubleQuotes(String string) {
+        if (string == null)
+            return null;
+        final int length = string.length();
+        if ((length > 1) && (string.charAt(0) == '"') && (string.charAt(length - 1) == '"')) {
+            return string.substring(1, length - 1);
+        }
+        return string;
+    }
+
+    // Remove the period from the network name
+    public static String removeTrailingPeriod(String string) {
+        if (string == null)
+            return null;
+        final int length = string.length();
+        if (string.endsWith(".")) {
+            string.substring(0, length - 1);
+        }
+        return string;
+    }
+}

packages/SystemUI/src/com/android/systemui/statusbar/toggles/BluetoothToggle.java
@@ -1,0 +1,92 @@
+
+package com.android.systemui.statusbar.toggles;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.view.View;
+
+import com.android.systemui.R;
+
+public class BluetoothToggle extends StatefulToggle {
+
+    public void init(Context c, int style) {
+        super.init(c, style);
+
+        BluetoothAdapter bt = BluetoothAdapter.getDefaultAdapter();
+        if (bt == null) {
+            return;
+        }
+        boolean enabled = bt.isEnabled();
+        setIcon(enabled ? R.drawable.ic_qs_bluetooth_on : R.drawable.ic_qs_bluetooth_off);
+        setLabel(enabled ? R.string.quick_settings_bluetooth_label
+                : R.string.quick_settings_bluetooth_off_label);
+        updateCurrentState(enabled ? State.ENABLED : State.DISABLED);
+
+        registerBroadcastReceiver(new BroadcastReceiver() {
+
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                int state = intent.getIntExtra(
+                        BluetoothAdapter.EXTRA_STATE,
+                        BluetoothAdapter.STATE_OFF);
+
+                String label = null;
+                int iconId = 0;
+                State newState = getState();
+                switch (state) {
+                    case BluetoothAdapter.STATE_CONNECTED:
+                    case BluetoothAdapter.STATE_CONNECTING:
+                        // don't care about these right now
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_ON:
+                        iconId = R.drawable.ic_qs_bluetooth_not_connected;
+                        label = mContext.getString(R.string.quick_settings_bluetooth_label);
+                        break;
+                    case BluetoothAdapter.STATE_ON:
+                        iconId = R.drawable.ic_qs_bluetooth_not_connected;
+                        label = mContext.getString(R.string.quick_settings_bluetooth_label);
+                        newState = State.ENABLED;
+                        break;
+                    case BluetoothAdapter.STATE_DISCONNECTED:
+                    case BluetoothAdapter.STATE_DISCONNECTING:
+                        // don't care about these right now
+                        break;
+                    case BluetoothAdapter.STATE_OFF:
+                        iconId = R.drawable.ic_qs_bluetooth_off;
+                        label = mContext.getString(R.string.quick_settings_bluetooth_off_label);
+                        newState = State.DISABLED;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        iconId = R.drawable.ic_qs_bluetooth_off;
+                        label = mContext.getString(R.string.quick_settings_bluetooth_off_label);
+                        break;
+                }
+                if (label != null && iconId > 0) {
+                    setInfo(label, iconId);
+                    scheduleViewUpdate();
+                    updateCurrentState(newState);
+                }
+            }
+        }, new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED));
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        Intent intent = new Intent(android.provider.Settings.ACTION_BLUETOOTH_SETTINGS);
+        startActivity(intent);
+        return super.onLongClick(v);
+    }
+
+    @Override
+    protected void doEnable() {
+        BluetoothAdapter.getDefaultAdapter().enable();
+    }
+
+    @Override
+    protected void doDisable() {
+        BluetoothAdapter.getDefaultAdapter().disable();
+    }
+
+}

nazunamoe/android_frameworks_base

Conflicts:
	packages/SystemUI/res/layout/quick_settings_tile_bugreport.xml
	packages/SystemUI/res/layout/quick_settings_tile_ime.xml
	packages/SystemUI/res/layout/quick_settings_tile_settings.xml
	packages/SystemUI/res/values/colors.xml
	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsModel.java
	packages/SystemUI/src/com/android/systemui/statusbar/tablet/NotificationPanel.java
	core/java/android/provider/Settings.java
	packages/SystemUI/res/layout/quick_settings_tile_location.xml
	packages/SystemUI/res/layout/toggle_tile_user.xml
	packages/SystemUI/res/values/strings.xml
	packages/SystemUI/res/values/styles.xml
	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

packages/SystemUI/src/com/android/systemui/statusbar/toggles/BaseToggle.java
@@ -1,0 +1,221 @@
+
+package com.android.systemui.statusbar.toggles;
+
+import android.app.ActivityManagerNative;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.internal.statusbar.IStatusBarService;
+import com.android.systemui.R;
+import com.android.systemui.statusbar.phone.QuickSettingsTileView;
+
+import java.util.ArrayList;
+
+public abstract class BaseToggle
+        implements OnClickListener, OnLongClickListener {
+
+    public static final String TAG = "Toggle";
+
+    protected Context mContext;
+
+    protected int mStyle;
+
+    private Drawable mIconDrawable = null;
+    private CharSequence mLabelText = null;
+    private int mTextSize = 12;
+
+    protected CompoundButton mToggleButton = null;
+    protected TextView mLabel = null;
+    protected ImageView mIcon = null;
+    private int mIconId = -1;
+
+    protected ArrayList<BroadcastReceiver> mRegisteredReceivers = new ArrayList<BroadcastReceiver>();
+
+    protected Handler mHandler;
+    private Runnable mUpdateViewRunnable = new Runnable() {
+        @Override
+        public void run() {
+            updateView();
+        }
+    };
+
+    public BaseToggle() {
+    }
+
+    protected void init(Context c, int style) {
+        mContext = c;
+        mStyle = style;
+        mHandler = new Handler();
+        setTextSize(ToggleManager.getTextSize(mContext));
+        scheduleViewUpdate();
+    }
+
+    protected final void setTextSize(int s) {
+        mTextSize = s;
+    }
+
+    protected final void setInfo(final String label, final int resId) {
+        setLabel(label);
+        setIcon(resId);
+    }
+
+    protected final void setLabel(final String label) {
+        mLabelText = label;
+    }
+
+    protected final void setLabel(final int labelRes) {
+        mLabelText = mContext.getText(labelRes);
+    }
+
+    protected final void setIcon(int resId) {
+        if (resId == mIconId) {
+            // a little cache action
+            return;
+        }
+        mIconDrawable = null;
+        mIconDrawable = mContext.getResources().getDrawable(resId);
+        mIconId = resId;
+    }
+
+    protected final void setIcon(Drawable d) {
+        mIconDrawable = d;
+        mIconId = -1;
+    }
+
+    protected void cleanup() {
+        mHandler.removeCallbacks(mUpdateViewRunnable);
+        for (BroadcastReceiver br : mRegisteredReceivers) {
+            mContext.unregisterReceiver(br);
+        }
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        return true;
+    }
+
+    protected final void collapseStatusBar() {
+        try {
+            IStatusBarService sb = IStatusBarService.Stub.asInterface(ServiceManager
+                    .getService(Context.STATUS_BAR_SERVICE));
+            sb.collapsePanels();
+        } catch (RemoteException e) {
+        }
+    }
+
+    protected final void dismissKeyguard() {
+        try {
+            ActivityManagerNative.getDefault().dismissKeyguardOnNextActivity();
+        } catch (RemoteException e) {
+        }
+    }
+
+    public QuickSettingsTileView createTileView() {
+        QuickSettingsTileView quick = (QuickSettingsTileView)
+                View.inflate(mContext, R.layout.toggle_tile, null);
+        quick.setVisibility(View.VISIBLE);
+        quick.setOnClickListener(this);
+        quick.setOnLongClickListener(this);
+        mLabel = (TextView) quick.findViewById(R.id.label);
+        mIcon = (ImageView) quick.findViewById(R.id.icon);
+        return quick;
+    }
+
+    public View createTraditionalView() {
+        View view = View.inflate(mContext, R.layout.toggle_traditional, null);
+        mLabel = (TextView) view.findViewById(R.id.label);
+        mIcon = (ImageView) view.findViewById(R.id.icon);
+        view.setOnClickListener(this);
+        view.setOnLongClickListener(this);
+        return view;
+
+    }
+
+    protected final void scheduleViewUpdate() {
+        mHandler.removeCallbacks(mUpdateViewRunnable);
+        mHandler.postDelayed(mUpdateViewRunnable, 100);
+    }
+
+    protected final void startActivity(String a) {
+        startActivity(new Intent(a));
+    }
+
+    protected final void startActivity(Intent i) {
+        collapseStatusBar();
+        dismissKeyguard();
+        mContext.startActivityAsUser(i, new UserHandle(UserHandle.USER_CURRENT));
+    }
+
+    protected final void registerBroadcastReceiver(BroadcastReceiver r, IntentFilter f) {
+        if (r == null) {
+            return;
+        }
+        mRegisteredReceivers.add(r);
+        mContext.registerReceiver(r, f);
+    }
+
+    protected void updateView() {
+        if (mStyle == ToggleManager.STYLE_SWITCH) {
+
+        } else if (mStyle == ToggleManager.STYLE_TILE) {
+
+            if (mLabel != null) {
+                mLabel.setText(mLabelText);
+                mLabel.setVisibility(View.VISIBLE);
+                // if (mIconDrawable != null) {
+                // mLabel.setCompoundDrawablesWithIntrinsicBounds(null,
+                // mIconDrawable, null, null);
+                // }
+                mLabel.setTextSize(1, mTextSize);
+            }
+            if (mIcon != null) {
+                if (mIconDrawable != null) {
+                    mIcon.setImageDrawable(mIconDrawable);
+                }
+            }
+
+        } else if (mStyle == ToggleManager.STYLE_TRADITIONAL) {
+            if (mIcon != null) {
+                if (mIconDrawable != null) {
+                    mIcon.setImageDrawable(mIconDrawable);
+                }
+            }
+        }
+    }
+
+    // Remove the double quotes that the SSID may contain
+    public static String removeDoubleQuotes(String string) {
+        if (string == null)
+            return null;
+        final int length = string.length();
+        if ((length > 1) && (string.charAt(0) == '"') && (string.charAt(length - 1) == '"')) {
+            return string.substring(1, length - 1);
+        }
+        return string;
+    }
+
+    // Remove the period from the network name
+    public static String removeTrailingPeriod(String string) {
+        if (string == null)
+            return null;
+        final int length = string.length();
+        if (string.endsWith(".")) {
+            string.substring(0, length - 1);
+        }
+        return string;
+    }
+}

packages/SystemUI/src/com/android/systemui/statusbar/toggles/BluetoothToggle.java
@@ -1,0 +1,103 @@
+
+package com.android.systemui.statusbar.toggles;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.view.View;
+
+import com.android.systemui.R;
+
+public class BluetoothToggle extends StatefulToggle {
+
+    public void init(Context c, int style) {
+        super.init(c, style);
+
+        BluetoothAdapter bt = BluetoothAdapter.getDefaultAdapter();
+        if (bt == null) {
+            return;
+        }
+        boolean enabled = bt.isEnabled();
+        setIcon(enabled ? R.drawable.ic_qs_bluetooth_not_connected : R.drawable.ic_qs_bluetooth_off);
+        setLabel(enabled ? R.string.quick_settings_bluetooth_label
+                : R.string.quick_settings_bluetooth_off_label);
+        updateCurrentState(enabled ? State.ENABLED : State.DISABLED);
+
+        registerBroadcastReceiver(new BroadcastReceiver() {
+
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                int state = intent.getIntExtra(
+                        BluetoothAdapter.EXTRA_STATE,
+                        BluetoothAdapter.STATE_OFF);
+
+                String label = null;
+                int iconId = 0;
+                State newState = getState();
+                switch (state) {
+                    case BluetoothAdapter.STATE_CONNECTED:
+                        iconId = R.drawable.ic_qs_bluetooth_on;
+                        label = mContext.getString(R.string.quick_settings_bluetooth_label);
+                        newState = State.ENABLED;
+                        break;
+                    case BluetoothAdapter.STATE_ON:
+                        iconId = R.drawable.ic_qs_bluetooth_not_connected;
+                        label = mContext.getString(R.string.quick_settings_bluetooth_label);
+                        newState = State.ENABLED;
+                        break;
+                    case BluetoothAdapter.STATE_CONNECTING:
+                    case BluetoothAdapter.STATE_TURNING_ON:
+                        iconId = R.drawable.ic_qs_bluetooth_not_connected;
+                        label = mContext.getString(R.string.quick_settings_bluetooth_label);
+                        newState = State.ENABLING;
+                        break;
+                    case BluetoothAdapter.STATE_DISCONNECTED:
+                        iconId = R.drawable.ic_qs_bluetooth_not_connected;
+                        label = mContext.getString(R.string.quick_settings_bluetooth_label);
+                        newState = State.DISABLED;
+                        break;
+                    case BluetoothAdapter.STATE_DISCONNECTING:
+                        iconId = R.drawable.ic_qs_bluetooth_not_connected;
+                        label = mContext.getString(R.string.quick_settings_bluetooth_label);
+                        newState = State.DISABLING;
+                        break;
+                    case BluetoothAdapter.STATE_OFF:
+                        iconId = R.drawable.ic_qs_bluetooth_off;
+                        label = mContext.getString(R.string.quick_settings_bluetooth_off_label);
+                        newState = State.DISABLED;
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        iconId = R.drawable.ic_qs_bluetooth_off;
+                        label = mContext.getString(R.string.quick_settings_bluetooth_off_label);
+                        newState = State.DISABLING;
+                        break;
+                }
+                if (label != null && iconId > 0) {
+                    setInfo(label, iconId);
+                    scheduleViewUpdate();
+                    updateCurrentState(newState);
+                }
+            }
+        }, new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED));
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        Intent intent = new Intent(android.provider.Settings.ACTION_BLUETOOTH_SETTINGS);
+        startActivity(intent);
+        return super.onLongClick(v);
+    }
+
+    @Override
+    protected void doEnable() {
+        BluetoothAdapter.getDefaultAdapter().enable();
+    }
+
+    @Override
+    protected void doDisable() {
+        BluetoothAdapter.getDefaultAdapter().disable();
+    }
+
+}