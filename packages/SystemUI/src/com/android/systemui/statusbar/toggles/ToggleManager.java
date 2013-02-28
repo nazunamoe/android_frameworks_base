e2a201011294f7eaa924aaa3048e9b638ee5c610
Roman Birg <romanbirg@gmail.com>
2013-02-28 10:37:23 -0600

    Toggle bug fixes
    - battery info should be stored
    - user/fav user should retain images after rotating
    - new fast charge file reading impl
    - bluetooth/wifiap logic improvements
    
    Change-Id: I7646292b88f34cd3b1e23ba615d2c571ca22cb89
    Signed-off-by: Roman Birg <romanbirg@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/toggles/FastChargeToggle.java

--- packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
@@ -243,6 +243,7 @@ public class ToggleManager {
                 if (networkController != null && toggle instanceof NetworkSignalChangedCallback) {
                     networkController
                             .addNetworkSignalChangedCallback((NetworkSignalChangedCallback) toggle);
+                    networkController.notifySignalsChangedCallbacks((NetworkSignalChangedCallback) toggle);
                 }
 
                 if (bluetoothController != null && toggle instanceof BluetoothStateChangeCallback) {
@@ -266,6 +267,9 @@ public class ToggleManager {
             } catch (Exception e) {
                 e.printStackTrace();
             }
+        }
+        if(batteryController != null) {
+            batteryController.updateCallbacks();
         }
     }
 
