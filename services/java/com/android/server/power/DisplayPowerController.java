b7ee1b0f665536a4ea380086cb9c4207e117ce16
Daniel <daxx4182@gmail.com>
2013-03-08 11:43:55 +0800

    Revert "CRT screen on/off animations user defined (1 of 2)"
    This reverts commit e266cb5eeba69f76295d942a923261947fa5728d.

--- services/java/com/android/server/power/DisplayPowerController.java
+++ services/java/com/android/server/power/DisplayPowerController.java
@@ -227,10 +227,6 @@ final class DisplayPowerController {
     // a stylish electron beam animation instead.
     private boolean mElectronBeamFadesConfig;
 
-    // Eos settings - override config for ElectronBeam on or off
-    private boolean mElectronBeamOnEnabled;
-    private boolean mElectronBeamOffEnabled;
-
     // The pending power request.
     // Initially null until the first call to requestPowerState.
     // Guarded by mLock.
@@ -666,10 +662,6 @@ final class DisplayPowerController {
             mustNotify = !mDisplayReadyLocked;
         }
 
-        // update crt settings here, it's only two bools
-        mElectronBeamOnEnabled = mPowerRequest.electronBeamOnEnabled;
-        mElectronBeamOffEnabled = mPowerRequest.electronBeamOffEnabled;
-
         // Initialize things the first time the power state is changed.
         if (mustInitialize) {
             initialize();
@@ -762,12 +754,12 @@ final class DisplayPowerController {
                         blockScreenOn();
                     } else {
                         unblockScreenOn();
-                        if (mElectronBeamOffEnabled) {
+                        if (USE_ELECTRON_BEAM_ON_ANIMATION) {
                             if (!mElectronBeamOnAnimator.isStarted()) {
                                 if (mPowerState.getElectronBeamLevel() == 1.0f) {
                                     mPowerState.dismissElectronBeam();
                                 } else if (mPowerState.prepareElectronBeam(
-                                        !mElectronBeamOnEnabled ?
+                                        mElectronBeamFadesConfig ?
                                                 ElectronBeam.MODE_FADE :
                                                         ElectronBeam.MODE_WARM_UP)) {
                                     mElectronBeamOnAnimator.start();
@@ -789,7 +781,7 @@ final class DisplayPowerController {
                         if (mPowerState.getElectronBeamLevel() == 0.0f) {
                             setScreenOn(false);
                         } else if (mPowerState.prepareElectronBeam(
-                                !mElectronBeamOffEnabled ?
+                                mElectronBeamFadesConfig ?
                                         ElectronBeam.MODE_FADE :
                                                 ElectronBeam.MODE_COOL_DOWN)
                                 && mPowerState.isScreenOn()) {
--- services/java/com/android/server/power/DisplayPowerRequest.java
+++ services/java/com/android/server/power/DisplayPowerRequest.java
@@ -62,10 +62,6 @@ final class DisplayPowerRequest {
     // visible to the user.
     public boolean blockScreenOn;
 
-    // Eos settings - override config for ElectronBeam on or off
-    public boolean electronBeamOnEnabled;
-    public boolean electronBeamOffEnabled;    
-
     public DisplayPowerRequest() {
         screenState = SCREEN_STATE_BRIGHT;
         useProximitySensor = false;
@@ -73,8 +69,6 @@ final class DisplayPowerRequest {
         screenAutoBrightnessAdjustment = 0.0f;
         useAutoBrightness = false;
         blockScreenOn = false;
-        electronBeamOnEnabled = false;
-        electronBeamOffEnabled = false;
     }
 
     public DisplayPowerRequest(DisplayPowerRequest other) {
@@ -88,8 +82,6 @@ final class DisplayPowerRequest {
         screenAutoBrightnessAdjustment = other.screenAutoBrightnessAdjustment;
         useAutoBrightness = other.useAutoBrightness;
         blockScreenOn = other.blockScreenOn;
-        electronBeamOnEnabled = other.electronBeamOnEnabled;
-        electronBeamOffEnabled = other.electronBeamOffEnabled;
     }
 
     @Override
@@ -105,9 +97,7 @@ final class DisplayPowerRequest {
                 && screenBrightness == other.screenBrightness
                 && screenAutoBrightnessAdjustment == other.screenAutoBrightnessAdjustment
                 && useAutoBrightness == other.useAutoBrightness
-                && blockScreenOn == other.blockScreenOn
-                && electronBeamOnEnabled == other.electronBeamOnEnabled
-                && electronBeamOffEnabled == other.electronBeamOffEnabled;
+                && blockScreenOn == other.blockScreenOn;
     }
 
     @Override
@@ -122,8 +112,6 @@ final class DisplayPowerRequest {
                 + ", screenBrightness=" + screenBrightness
                 + ", screenAutoBrightnessAdjustment=" + screenAutoBrightnessAdjustment
                 + ", useAutoBrightness=" + useAutoBrightness
-                + ", blockScreenOn=" + blockScreenOn
-                + ", electronBeamOnEnabled=" + electronBeamOnEnabled
-                + ", electronBeamOffEnabled=" + electronBeamOffEnabled;
+                + ", blockScreenOn=" + blockScreenOn;
     }
 }
