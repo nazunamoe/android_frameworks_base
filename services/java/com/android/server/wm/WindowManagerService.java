ac669498f251bdec5d9c3e48d7e210e14c61a389
Danny Baumann <dannybaumann@web.de>
2013-03-27 12:26:01 +0100

    Fix divisions by zero on boot.
    Change I976ffae3a56d411b00026f86b16efa9743a23293 introduced a problem
    where depending on the rotation settings the WM override display
    configuration would be filled with invalid values.
    Fix that by correctly checking that the display is ready prior to
    setting the override configuration.
    
    Change-Id: Ia3fd7603d9c1212cd32536803b4c12390a798606

--- services/java/com/android/server/wm/WindowManagerService.java
+++ services/java/com/android/server/wm/WindowManagerService.java
@@ -6962,7 +6962,11 @@ public class WindowManagerService extends IWindowManager.Stub
 
 	private ApplicationDisplayMetrics updateApplicationDisplayMetricsLocked(
             DisplayContent displayContent) {
-		final ApplicationDisplayMetrics m = calculateDisplayMetrics(displayContent);
+                if (!mDisplayReady) {
+                    return null;
+                }
+
+        final ApplicationDisplayMetrics m = calculateDisplayMetrics(displayContent);
         final int appWidth = mPolicy.getNonDecorDisplayWidth(m.dw, m.dh, mRotation);
         final int appHeight = mPolicy.getNonDecorDisplayHeight(m.dw, m.dh, mRotation);
         final DisplayInfo displayInfo = displayContent.getDisplayInfo();
@@ -6989,16 +6993,17 @@ public class WindowManagerService extends IWindowManager.Stub
     }
 
     boolean computeScreenConfigurationLocked(Configuration config) {
-        if (!mDisplayReady) {
-            return false;
-        }
-
         // TODO(multidisplay): For now, apply Configuration to main screen only.
         final DisplayContent displayContent = getDefaultDisplayContentLocked();
 
         // Update application display metrics.
         final ApplicationDisplayMetrics appDm = updateApplicationDisplayMetricsLocked(
                 displayContent);
+
+        if (appDm == null) {
+            return false;
+        }
+
         final boolean rotated = appDm.rotated;
         final int dw = appDm.dw;
         final int dh = appDm.dh;
