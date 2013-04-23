9728c8439060e5de26e866e0aae0265e8aca70eb
Stevespear426 <stephen.k.spear@gmail.com>
2013-04-23 02:44:41 +0200

    FW: Ribbons Padding, Click options, autohide updates 1/2
    Adjustable padding
    vibrate on click optional
    if no short action set use long action icon and
      dont set short click listener - long press only mode...
    Autohide flakiness fixed
    Don't unlock on torch
    
    Change-Id: I45c9f29453220e80249300b66bb61d5a7afc784f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2043,6 +2043,31 @@ public final class Settings {
             "ribbon_swipe_right",
         };
 
+        public static final String[] RIBBON_ICON_SPACE = new String[] {
+            "ribbon_icon_lockscreen_space",
+            "ribbon_icon_notification_space",
+            "ribbon_icon_swipe_space",
+            "ribbon_icon_quicksettings_space",
+        };
+
+        /**
+         *
+         * @hide
+         */
+        public static final String RIBBON_ICON_SPACE_VERTICAL = "ribbon_icon_space_vertical";
+
+        /**
+         * Ribbon Targets
+         *
+         * @hide
+         */
+        public static final String[] RIBBON_ICON_VIBRATE = new String[] {
+            "ribbon_icon_lockscreen_vibrate",
+            "ribbon_icon_notification_vibrate",
+            "ribbon_icon_swipe_vibrate",
+            "ribbon_icon_quicksettings_vibrate",
+        };
+
         /**
          *
          * @hide
