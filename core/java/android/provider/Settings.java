feb27d7a9c0fe5595b9f27500ce95583b2be91a4
tonyp <tonyptonyp@gmail.com>
2013-06-12 23:33:45 +0200

    HALO: Option to pause the underlying app (1/2)
    - adds an option to pause the active app when using HALO
    - this is enabled by default for devices with low memory
    - (currently: <640MB RAM available to the kernel)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4150,6 +4150,13 @@ public final class Settings {
         public static final String HALO_HIDE = "halo_hide";
 
         /**
+         * HALO pause activities?, defaults to 0 (no, do not pause) on devices which isLargeRAM() == true
+         * otherwise it defaults to 1 (yes, do pause)
+         * @hide
+         */
+        public static final String HALO_PAUSE = "halo_pause";
+
+        /**
          * Whether Status Bar is currently hidden or not for notification
          * toggle notification shade
          *
