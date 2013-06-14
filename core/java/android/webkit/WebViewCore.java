63d64173e5b356090c7214f684f1b22186acb847
Daniel MM <daxx4182@gmail.com>
2013-06-14 18:04:36 +0800

    Revert "Merge scrollfixthing"
    This reverts commit cb3344f691d464ad99da83a1934d6e66a9b903e0.

--- core/java/android/webkit/WebViewCore.java
+++ core/java/android/webkit/WebViewCore.java
@@ -2530,11 +2530,7 @@ public final class WebViewCore {
         // adjust the default scale to match the densityDpi
         float adjust = 1.0f;
         if (mViewportDensityDpi == -1) {
-		if( mWebViewClassic != null && mWebViewClassic.getDefaultZoomScale() * mSettings.getDefaultZoom().value != mContext.getResources().getDisplayMetrics().density * 100)
-        		mWebViewClassic.adjustDefaultZoomDensity( mSettings.getDefaultZoom().value);      
-           		 if( mWebViewClassic != null && (int)( mWebViewClassic.getDefaultZoomScale() * 100) != 100) {
-           adjust = mWebViewClassic.getDefaultZoomScale();
-           }
+            adjust = getFixedDisplayDensity(mContext);
         } else if (mViewportDensityDpi > 0) {
             adjust = (float) mContext.getResources().getDisplayMetrics().densityDpi
                     / mViewportDensityDpi;
